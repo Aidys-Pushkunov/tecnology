aad8674b9cce30c2d91c8bff68d201f6c61abf41
Aidys-Pushkunov <62947297+Aidys-Pushkunov@users.noreply.github.com>
2021-01-10 20:17:44 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let menu = document.getElementById('menu');
 let count = 0;
-let image = ["img/slidbar/1 (1).JPG", "img/slidbar/1 (2).JPG", "img/slidbar/1 (3).JPG"];
+let image = ["img/slidbar/1 (1).JPEG", "img/slidbar/1 (1).JPG", "img/slidbar/1 (1).png", "img/slidbar/1 (2).JPG", "img/slidbar/1 (3).JPG", "img/slidbar/1 (3).png", "img/slidbar/1 (4).JPG", "img/slidbar/1 (5).jpg", "img/slidbar/1 (6).jpg", "img/slidbar/1 (7).JPG", "img/slidbar/1 (8).jpg", "img/slidbar/1 (9).JPG"]
 let num = 0;
 
 function myFunction() {
